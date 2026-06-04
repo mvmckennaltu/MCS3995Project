aae0b1d3345167765e2679612b7d3a2628f06620
mvmckennaltu <142845590+mvmckennaltu@users.noreply.github.com>
2026-06-03 22:00:43 -0400

    Did some BS

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,27 +7,22 @@ extends CharacterBody3D
 var player_health = 99
 var target_velocity = Vector3.ZERO
 var input_direction = Vector2.ZERO
-
+enum PlayerState {
+	IDLE,
+	RUN,
+	JUMP,
+	FALL
+}
+var state = PlayerState.IDLE
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	# We check for each move input and update the direction accordingly.
-	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down", 0.15)
 	direction.x = input_direction.x
 	direction.z = input_direction.y
-	#if Input.is_action_pressed("move_right"):
-		#direction.x += 1
-	#if Input.is_action_pressed("move_left"):
-		#direction.x -= 1
-	#if Input.is_action_pressed("move_down"):
-		# Notice how we are working with the vector's x and z axes.
-		# In 3D, the XZ plane is the ground plane.
-		#direction.z += 1
-	#if Input.is_action_pressed("move_up"):
-		#direction.z -= 1
 	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		# Setting the basis property will affect the rotation of the node.
-		$Pivot.basis = Basis.looking_at(direction)
+			var facing_dir = direction.normalized()
+			$Pivot.basis = Basis.looking_at(facing_dir)
 	# Ground Velocity
 	target_velocity.x = direction.x * run_speed
 	target_velocity.z = direction.z * run_speed
@@ -39,10 +34,31 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
-	print("Input_direction " , direction.x," ", direction.z)
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		jump()
-
+	if is_on_floor():
+		if input_direction.length() > 0.1:
+			state = PlayerState.RUN
+		else:
+			state = PlayerState.IDLE
+		
+	match state:
+		PlayerState.IDLE:
+			play_anim("Player/idle")
+		PlayerState.RUN:
+			play_anim("Player/running")
+		PlayerState.FALL:
+			play_anim("Player/idle")
 func jump():
 	target_velocity.y = jump_impulse
+	state = PlayerState.JUMP
+	play_anim("Player/jumpup")
+
+func play_anim(anim_name):
+	var anim = $Pivot/PlayerModel/AnimationPlayer
+	if anim.current_animation != anim_name:
+		anim.play(anim_name)
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "Player/jumpup":
+		state = PlayerState.FALL
